ecf2d16f0609702f358b070f2e15fab43536f84f
Sebastian <sebastianlopez285@gmail.com>
2024-10-02 20:05:16 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function sumar(){
+    let num1 = parseInt(prompt("ingresar un nro"))
+    let num2 = parseInt(prompt("ingresar otro nro"))
+    let suma = num1 + num2;
+    console.log("el total de productos(2) y total a pagar es $" + suma );
+
+}
+sumar();
+
+function carrito(){
+    console.log("completar carrito de compras")
+    console.log("completar carrito de compras")
+    console.log("completar carrito de compras")
+    console.log("completar carrito de compras")
+    console.log("completar carrito de compras")
+    console.log("completar carrito de compras")
+    console.log("completar carrito de compras")
+}
+
+carrito();
+carrito();
